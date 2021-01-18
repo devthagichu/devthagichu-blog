b847e429abc7313ad29145fece37e9a5af9d5d0b
Anthony Thagichu <athagichu@gerties.org>
2021-01-18 15:53:52 +0300

    added author to the blog , contacnts and about

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,8 +8,8 @@ import Head from "../components/head/Head"
 const IndexPage = () => {
 
 const data = useStaticQuery(graphql`
-  query MyQuery {
-  allContentfulArticle(filter: {node_locale: {eq: "en-US"}}) {
+query MyQuery {
+  allContentfulArticle(filter: {node_locale: {eq: "en-US"}}, sort: {fields: createdAt}) {
     edges {
       node {
         title
@@ -20,17 +20,32 @@ const data = useStaticQuery(graphql`
         createdAt(formatString: "Do MMMM YYYY")
         slug
         id
-         image {
+        image {
           fluid {
             src
             base64
             sizes
           }
         }
+        author {
+          id
+          name
+          twitter
+          title
+          avator {
+            fluid {
+              src
+              base64
+              sizes
+            }
+          }
+        }
       }
     }
   }
 }
+
+
 `)
 
 
@@ -54,9 +69,18 @@ const data = useStaticQuery(graphql`
                     }
                 <Img fluid={edge.node.image.fluid} alt={edge.node.title} className={blogStyles.card__image}/>
                 </div>
+                <p className={blogStyles.card__date}>{edge.node.createdAt}</p>
                 <h3 className={blogStyles.card__title}>{edge.node.title}</h3>
                 </Link>
-                <p className={blogStyles.card__date}>{edge.node.createdAt}</p>
+                <div style={{display:"flex", marginTop:"1.2rem", marginBottom:"1.2rem"}}>
+                  <div style={{marginRight:12}}>
+                  <Img fluid={edge.node.author.avator.fluid} alt={edge.node.author.name} style={{width:38, height:38}}/>
+                  </div>
+                  <div>
+                    <h4>{edge.node.author.name}</h4>
+                    <h5>{edge.node.author.title}</h5>
+                  </div>
+                </div>
                 <p className={blogStyles.card__body}>Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </p>
             </div>))
             }
---
+++ src/pages/contacts.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import PageLayout from "../components/page/PageLayout"
+import Head from "../components/head/Head"
+
+const Contact = () => {
+  return (
+ 
+    <PageLayout>
+      <Head title="Home" />
+      <div>
+        home
+      </div>
+    </PageLayout>
+ 
+  )
+}
+
+export default Contact
